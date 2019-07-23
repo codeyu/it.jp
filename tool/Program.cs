e08a9c9856610a786cd813c58722407bab040027
codeyu <codeyu@gmail.com>
2019-07-23 19:20:48 +0900

    up tool

---
+++ tool/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lst = new List<string>();
+            string[] lines =  System.IO.File.ReadAllLines(@"..\it.jp.txt");
+            foreach (string line in lines)
+            {
+                var tabs = line.Split("     ").Select(x=>x.Trim()).ToList();
+                if(tabs[0].Trim() == "id")
+                {
+                    var tableHead = string.Join('|', tabs);
+                    lst.Add(tableHead);
+                    Console.WriteLine(tableHead);
+                    lst.Add("--- | --- | --- | ---");
+                    Console.WriteLine("--- | --- | --- | ---");
+                }
+                else
+                {
+                    var row = string.Join('|', tabs);
+                    lst.Add(row);
+                    Console.WriteLine(row);
+                }
+                
+            }
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
